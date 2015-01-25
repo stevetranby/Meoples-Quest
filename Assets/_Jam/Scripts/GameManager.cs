d264802d1f4cbb84648ce5ab1b1f544c21fff674
Steve Tranby <stevetranby@gmail.com>
2015-01-25 09:38:19 -0700

    starting work on cutscenes

--- Assets/_Jam/Scripts/GameManager.cs
+++ Assets/_Jam/Scripts/GameManager.cs
@@ -1,53 +1,133 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
 // TODO: make sure this can persist across level scenes?
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
     public GameObject currentPlayer;
     public List<GameObject> players;
+    public bool inCutscene;
+    private float cutSceneFailsafeTimer = 1f;
+    private Text cutSceneMessageHUD;
+    private StoryTrigger currentStory;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {        
+        cutSceneMessageHUD = GameObject.Find("cutSceneMessageHUD").GetComponent<Text>();
+
         players = new List<GameObject>();
-        for(int i = 0; i < 6; ++i)
+        for (int i = 0; i < 6; ++i)
         {
-            var player = GameObject.Find("Player" + (i+1));
-            if(player) {
+            var player = GameObject.Find("Player" + (i + 1));
+            if (player)
+            {
                 players.Add(player);
             }
         }
         setActivePlayer(1);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-	}
-
-    public void setActivePlayer(int playerNum) 
+    }
+    
+    // Update is called once per frame
+    void Update()
     {
+        if (this.inCutscene)
+        {
+            Debug.Log("cutscene time left: " + cutSceneFailsafeTimer);
+            // continue cutscene
+            //TODO: StartCoroutine();
+            if (cutSceneFailsafeTimer <= 0)
+            {
+                Debug.Log("cutscene time OVER!");
+                cutSceneFailsafeTimer = 0f;
+                this.exitCutscene();
+            }
+            cutSceneFailsafeTimer -= Time.deltaTime;
+        } 
+    }
+
+    public void enterCutscene(StoryTrigger story)
+    {
+        currentStory = story;
+        StartCoroutine("enterCutsceneCoroutine");
+    }
+
+    public IEnumerator enterCutsceneCoroutine()
+    {
+        Debug.Log("enterting cutscene");
+        inCutscene = true;
+
+        cutSceneFailsafeTimer = currentStory.storyDuration;
+
+        // story props
+        cutSceneMessageHUD.text = currentStory.Message;
+
+        if(currentStory.StoryID == 2) {
+            // lever cutscene
+            //setActivePlayer(1);
+            // follow camera instead
+            var follow = Camera.main.GetComponent<CameraFollow>();
+            follow.playerTransform = currentPlayer.transform;
+            var player2 = players[1];
+            if(player2) {
+                follow.playerTransform = player2.transform;
+            }
+
+            yield return new WaitForSeconds(0.2f);
+        }
+
+        removeCurrentPlayerControl();
+    }
+
+    public void exitCutscene()
+    {
+        if(! inCutscene) { return; }
+        Debug.Log("exiting cutscene");
+        inCutscene = false;
+        setActivePlayer(1);
+    }
+
+    public void setActivePlayer(int playerNum)
+    {
+        if (inCutscene)
+        {
+            return;
+        } 
+
         var arrayIndex = playerNum - 1;
-        var player = arrayIndex < players.Count ? players[arrayIndex] : null;
+        var player = arrayIndex < players.Count ? players [arrayIndex] : null;
 
         Debug.Log("setting player " + arrayIndex + " to active!");
 
-        if(player) {
+        if (player)
+        {
             // remove controller from previous player
-            if(currentPlayer) 
-            {
-                var control = currentPlayer.GetComponent<PlayerControl>();
-                if(control) {  control.enabled = false; }
-            }
+            removeCurrentPlayerControl();
 
             currentPlayer = player;
 
+            var control = currentPlayer.GetComponent<PlayerControl>();
+            if (control)
             {
-                var control = currentPlayer.GetComponent<PlayerControl>();
-                if(control) { control.enabled = true; }
-                var follow = Camera.main.GetComponent<CameraFollow>();
-                follow.playerTransform = currentPlayer.transform;
+                control.enabled = true;
+            }
+            var follow = Camera.main.GetComponent<CameraFollow>();
+            follow.playerTransform = currentPlayer.transform;
+        }
+    }
+
+    public void removeCurrentPlayerControl()
+    {
+        if (currentPlayer)
+        {
+            var control = currentPlayer.GetComponent<PlayerControl>();
+            if (control)
+            {
+                Debug.Log("disabling player control");
+                control.enabled = false;
             }
         }
     }
--- Assets/_Jam/Scripts/StoryTrigger.cs
+++ Assets/_Jam/Scripts/StoryTrigger.cs
@@ -1,45 +1,63 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class StoryTrigger : MonoBehaviour {
+public class StoryTrigger : MonoBehaviour
+{
 
     public int StoryID;
     public string Message = "";
     public string SpeechIcon = "";
-    public GameObject [] SpeechIcons;
-    public AudioClip [] TalkingClips;
+    public GameObject[] SpeechIcons;
+    public AudioClip[] TalkingClips;
     public bool disablePlayerControl;
+    private GameManager gmgr;   
+    public float storyDuration = 2f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     public void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Player") 
+        if (other.tag == "Player")
         {
+            Debug.Log("story trigger activated!");
+
+            var managerGo = GameObject.FindGameObjectWithTag("GameManager");
+            gmgr = managerGo.GetComponent<GameManager>();
+            gmgr.enterCutscene(this);
+
+            this.enabled = false;
+
             // do story
-            if(StoryID == 3) {
+            if (StoryID == 3)
+            {
                 // perform cutscene balloon
-            } else {
-
+            } 
+            else
+            {
                 // show message bubbles
-
 
                 // play random clip
                 // Play the new taunt.
-                if(TalkingClips.Length > 0) {
-                    int index = Random.Range(0,TalkingClips.Length);
-                    audio.clip = TalkingClips[index];
+                if (TalkingClips.Length > 0)
+                {
+                    int index = Random.Range(0, TalkingClips.Length);
+                    audio.clip = TalkingClips [index];
                     audio.Play();
                 }
             }
+
+            Destroy(this.gameObject);
         }
+       
     }
 }
