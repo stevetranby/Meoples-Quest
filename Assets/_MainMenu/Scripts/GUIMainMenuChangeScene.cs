718615f262a7fd8c1900cc25754bb3d0b1199be7
Steve Tranby <stevetranby@gmail.com>
2015-01-25 10:39:09 -0700

    adding in our main menu scene

---
+++ Assets/_MainMenu/Scripts/GUIMainMenuChangeScene.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIMainMenuChangeScene : MonoBehaviour {
+
+
+	
+	// Update is called once per frame
+	public void ChangeToScene (string SceneToChangeTo) {
+		Application.LoadLevel (SceneToChangeTo);
+		//scene to change too ^
+
+	}
+}
---
+++ Assets/_MainMenu/Scripts/VolumeControlScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VolumeControlScript : MonoBehaviour {
+	public float hSliderValue = 0.0F;
+	// Use this for initialization
+
+	void OnGUI() {
+//		hSliderValue = GUI.HorizontalSlider(new Rect(1185, 590, 100, 30), hSliderValue, 0.0F, 10.0F);
+//		AudioListener.volume = hSliderValue;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+
+}
